25b8f8c3ab43a2944cc95c6232b3721d2ef81956
Soliquidus <tibo.pfeifer@gmail.com>
2021-11-05 11:31:07 +0100

    TP 01

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Diginamic-Test-Jpa" 
---
+++ src/main/java/fr/diginamic/testjpa/TestJpa.java
@@ -0,0 +1,23 @@
+package fr.diginamic.testjpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestJpa {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( TestJpa.class );
+
+    public static void main( String[] args ) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory( "testjpa" );
+        EntityManager em = emf.createEntityManager();
+        System.out.println(em);
+        em.close();
+        emf.close();
+        LOGGER.info( "Hello, world!!" );
+    }
+}
